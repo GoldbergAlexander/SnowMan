085cd3501c5c9f7516a3338b95c5df69527a6d0e
Happimir <kovalsky@uga.edu>
2015-04-09 21:39:02 -0400

    Read the TODO for directions.

--- src/AnimateSnowman.java
+++ src/AnimateSnowman.java
@@ -1,33 +1,24 @@
-    import com.sun.scenario.effect.Glow;
-    import javafx.animation.KeyFrame;
-    import javafx.animation.KeyValue;
-    import javafx.animation.Timeline;
-    import javafx.application.Application;
-	import javafx.event.ActionEvent;
-	import javafx.geometry.Insets;
-    import javafx.geometry.Pos;
-    import javafx.scene.Scene;
-    import javafx.scene.control.Button;
-    import javafx.scene.layout.HBox;
-    import javafx.scene.layout.Pane;
-    import javafx.scene.layout.BorderPane;
-    import javafx.scene.paint.Color;
-    import javafx.scene.shape.*;
-    import javafx.stage.Stage;
-    import javafx.util.Duration;
-    import javafx.scene.Group;
-    import javafx.scene.Node;
-	import javafx.event.EventHandler;
-    import javafx.scene.effect.*;
 
-	import java.util.Objects;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
-    public class AnimateSnowman extends Application
+public class AnimateSnowman extends Application
 {
-	final int SCENE_WIDTH = 500;
-	final int SCENE_HEIGHT = 350;
-	
+	final int SCENE_WIDTH = 700;
+	final int SCENE_HEIGHT = 400;
+
 	static Circle head = new Circle();
 	static Circle chest = new Circle();
 	static Circle bottom = new Circle();
@@ -35,48 +26,82 @@
 	static Circle rightEye = new Circle();
 	static Circle sun = new Circle();
 	static Circle heart = new Circle();
-	
+
 	static Ellipse leftArm = new Ellipse();
 	static Ellipse rightArm = new Ellipse();
-	
+
 	static Arc mouth = new Arc();
+	static Arc moon = new Arc();
 
+//	static Button startAnimation = new Button();
+	//static Button exitAnimation = new Button();
 
+	static Rectangle night = new Rectangle();
 
-    Group snowManGroup = new Group();
-	
 	static Pane animationPane = new Pane();
-	
+
 	static HBox hbox = new HBox();
-	
+
 	static Timeline timeLine;
-	
+
 	private void drawSun()
 	{
-		sun.setCenterX(SCENE_WIDTH);
-		sun.setCenterY(0);
-		sun.setRadius(75);		
+		sun.setRadius(50);
+		sun.setCenterX(SCENE_WIDTH - (sun.getRadius() + 5));
+		sun.setCenterY(SCENE_HEIGHT + sun.getRadius());
 		sun.setFill(Color.YELLOW);
+
+		timeLine = new Timeline();
+		timeLine.setCycleCount(1);
+		timeLine.setAutoReverse(false);
+
+		KeyValue sunValue = new KeyValue(sun.translateYProperty(), (-SCENE_HEIGHT + 5));
+		KeyFrame keyFrame  = new KeyFrame(Duration.millis(6000), sunValue);
+
+		timeLine.getKeyFrames().addAll(keyFrame);
+		timeLine.play();
 	}
-	
+
+	private void drawMoon()
+	{
+		moon.setRadiusX(35);
+		moon.setRadiusY(35);
+		moon.setType(ArcType.OPEN);
+		moon.setCenterX(moon.getRadiusX() + 5);
+		moon.setCenterY(moon.getRadiusY());
+		moon.setStartAngle(135);
+		moon.setLength(180);
+		moon.setStrokeType(StrokeType.INSIDE);
+		moon.setFill(Color.WHEAT);
+
+		timeLine = new Timeline();
+		timeLine.setCycleCount(1);
+		timeLine.setAutoReverse(false);
+
+		KeyValue moonValue = new KeyValue(moon.translateYProperty(), (SCENE_HEIGHT + moon.getCenterY()));
+		KeyFrame keyFrame  = new KeyFrame(Duration.millis(6000), moonValue);
+
+		timeLine.getKeyFrames().addAll(keyFrame);
+		timeLine.play();
+	}
+
 	private void drawHead()
 	{
 		head.setCenterX(SCENE_WIDTH / 2);
-		head.setCenterY(135);
-		head.setRadius(25);		
+		head.setCenterY(SCENE_HEIGHT - 215);
+		head.setRadius(25);
 		head.setFill(Color.AQUAMARINE);
 	}
 	private void drawMouth()
 	{
 		mouth.setCenterX(SCENE_WIDTH / 2);
-		mouth.setCenterY(95);
+		mouth.setCenterY(SCENE_HEIGHT - 255);
 		mouth.setLength(30);
 		mouth.setRadiusX(30);
 		mouth.setRadiusY(53);
 		mouth.setStartAngle(255);
 		mouth.setFill(Color.BLACK);
 
-		
 		timeLine = new Timeline();
 		timeLine.setCycleCount(Timeline.INDEFINITE);
 		timeLine.setAutoReverse(true);
@@ -85,16 +110,16 @@
 		KeyValue mouthYValue = new KeyValue(mouth.scaleYProperty(), 2);
 		KeyFrame keyFrameX  = new KeyFrame(Duration.millis(1000), mouthXValue);
 		KeyFrame keyFrameY  = new KeyFrame(Duration.millis(1000), mouthYValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrameX, keyFrameY); 
+
+		timeLine.getKeyFrames().addAll(keyFrameX, keyFrameY);
 		timeLine.play();
 	}
-	
+
 	private void drawLeftEye()
 	{
 		leftEye.setCenterX(SCENE_WIDTH/2 + 7);
-		leftEye.setCenterY(130);
-		leftEye.setRadius(4);		
+		leftEye.setCenterY(SCENE_HEIGHT - 220);
+		leftEye.setRadius(4);
 		leftEye.setFill(Color.BLUE);
 
 		timeLine = new Timeline();
@@ -103,44 +128,44 @@
 
 		KeyValue leftEyeValue = new KeyValue(leftEye.centerXProperty(), SCENE_WIDTH/2 + 4);
 		KeyFrame keyFrame  = new KeyFrame(Duration.millis(1000), leftEyeValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrame); 
+
+		timeLine.getKeyFrames().addAll(keyFrame);
 		timeLine.play();
 	}
-	
+
 	private void drawRightEye()
 	{
 		rightEye.setCenterX(SCENE_WIDTH/2 - 7);
-		rightEye.setCenterY(130);
-		rightEye.setRadius(4);		
+		rightEye.setCenterY(SCENE_HEIGHT -220);
+		rightEye.setRadius(4);
 		rightEye.setFill(Color.GREEN);
-		
+
 		timeLine = new Timeline();
 		timeLine.setCycleCount(Timeline.INDEFINITE);
 		timeLine.setAutoReverse(true);
 
 		KeyValue rightEyeValue = new KeyValue(rightEye.centerXProperty(), SCENE_WIDTH/2 - 4);
 		KeyFrame keyFrame  = new KeyFrame(Duration.millis(1000), rightEyeValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrame); 
+
+		timeLine.getKeyFrames().addAll(keyFrame);
 		timeLine.play();
 	}
-	
+
 	private void drawChest()
 	{
 		chest.setCenterX(SCENE_WIDTH/2);
-		chest.setCenterY(195);
-		chest.setRadius(45);		
+		chest.setCenterY(SCENE_HEIGHT - 155);
+		chest.setRadius(45);
 		chest.setFill(Color.AQUAMARINE);
 	}
-	
+
 	private void drawHeart()
 	{
 		heart.setCenterX(SCENE_WIDTH/2 + 15);
-		heart.setCenterY(190);
-		heart.setRadius(7);		
+		heart.setCenterY(SCENE_HEIGHT - 160);
+		heart.setRadius(7);
 		heart.setFill(Color.PURPLE);
-		
+
 		timeLine = new Timeline();
 		timeLine.setCycleCount(Timeline.INDEFINITE);
 		timeLine.setAutoReverse(true);
@@ -149,26 +174,26 @@
 		KeyValue heartYValue = new KeyValue(heart.scaleYProperty(), 1.5);
 		KeyFrame keyFrameX  = new KeyFrame(Duration.millis(500), heartXValue);
 		KeyFrame keyFrameY  = new KeyFrame(Duration.millis(500), heartYValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrameX, keyFrameY); 
+
+		timeLine.getKeyFrames().addAll(keyFrameX, keyFrameY);
 		timeLine.play();
 	}
-	
+
 	private void drawBottom()
 	{
 		bottom.setCenterX(SCENE_WIDTH / 2);
-		bottom.setCenterY(265);
-		bottom.setRadius(65);	
+		bottom.setCenterY(SCENE_HEIGHT - 85);
+		bottom.setRadius(65);
 		bottom.setFill(Color.AQUAMARINE);
 	}
-	
+
 	private void drawLeftArm()
 	{
 		leftArm.setRadiusX(50.0);
 		leftArm.setRadiusY(15.0);
 		leftArm.setCenterX((SCENE_WIDTH + leftArm.getRadiusX() + chest.getRadius()) / 2);
-		leftArm.setCenterY(175.0);
-		
+		leftArm.setCenterY(SCENE_HEIGHT - 175);
+
 		leftArm.setFill(Color.AQUAMARINE);
 
 		timeLine = new Timeline();
@@ -177,117 +202,79 @@
 
 		KeyValue leftArmValue = new KeyValue(leftArm.rotateProperty(), 10);
 		KeyFrame keyFrame  = new KeyFrame(Duration.millis(1000), leftArmValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrame); 
+
+		timeLine.getKeyFrames().addAll(keyFrame);
 		timeLine.play();
 	}
-	
+
 	private void drawRightArm()
 	{
 		rightArm.setRadiusX(50.0);
 		rightArm.setRadiusY(15.0);
 		rightArm.setCenterX((SCENE_WIDTH - rightArm.getRadiusX() - chest.getRadius()) / 2);
-		rightArm.setCenterY(175.0);
+		rightArm.setCenterY(SCENE_HEIGHT - 175);
 
 		rightArm.setFill(Color.AQUAMARINE);
-		
+
 		timeLine = new Timeline();
 		timeLine.setCycleCount(Timeline.INDEFINITE);
 		timeLine.setAutoReverse(true);
 
 		KeyValue rightArmValue = new KeyValue(rightArm.rotateProperty(), 10);
 		KeyFrame keyFrame  = new KeyFrame(Duration.millis(1000), rightArmValue);
-		
-		timeLine.getKeyFrames().addAll(keyFrame); 
-		timeLine.play();	
+
+		timeLine.getKeyFrames().addAll(keyFrame);
+		timeLine.play();
 	}
 
-    private void setUpBlending(){
-        snowManGroup.getChildren().add(head);
-        snowManGroup.getChildren().add(chest);
-        snowManGroup.getChildren().add(bottom);
-        snowManGroup.getChildren().add(leftEye);
-        snowManGroup.getChildren().add(rightEye);
-        snowManGroup.getChildren().add(leftArm);
-        snowManGroup.getChildren().add(rightArm);
-        Node[] nodes = (Node[])snowManGroup.getChildren().toArray();
-        for (Node n : nodes){
-          // n.setEffect();
-        }
+	public void drawNght()
+	{
+		night.setX(0);
+		night.setY(0);
+		night.setWidth(SCENE_WIDTH);
+		night.setHeight(SCENE_HEIGHT);
+		night.setFill(Color.BLACK);
 
-
-    }
+		FadeTransition ft = new FadeTransition(Duration.millis(6000), night);
+		ft.setFromValue(1.0);
+		ft.setToValue(0.0);
+		ft.setCycleCount(1);
+		ft.setAutoReverse(false);
+		ft.play();
+	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception 
+	public void start(Stage primaryStage) throws Exception
 	{
-		Button startAnimation = new Button();
-		Button exitAnimation = new Button();
+		//startAnimation.setText("Start Animation");
+		//exitAnimation.setText("Exit Animation");
 
-		BorderPane borderPane = new BorderPane();
-		HBox buttonHBox = new HBox();
+		drawNght();
+		drawSun();
+		drawMoon();
 
-		startAnimation.setText("Start Animation");
-		exitAnimation.setText("Exit Animation");
+		drawHead();
+		drawChest();
+		drawBottom();
+		drawHeart();
 
-		startAnimation.setOnAction(new EventHandler<ActionEvent>()
-		{
-			@Override
-			public void handle(ActionEvent event)
-			{
+		drawLeftEye();
+		drawRightEye();
+		drawMouth();
 
-				drawSun();
-				drawHead();
-				drawChest();
-				drawBottom();
-				drawHeart();
+		drawLeftArm();
+		drawRightArm();
 
-				drawLeftEye();
-				drawRightEye();
-				drawMouth();
-
-				drawLeftArm();
-				drawRightArm();
-
-			}
-		});
-
-		//TODO: Add an exit command to the exitAnimation button in order to allow the user to exit the program from within instead of clicking the X button.
-		//TODO: Furthermore, I added an action event for the startAnimation button for it to start up the animation, could be better.
-
-
-
-		animationPane.getChildren().addAll(sun, head, chest, bottom, leftArm, rightArm, heart, leftEye, rightEye, mouth);
-		animationPane.setStyle("-fx-background-color: ghostwhite");
-
-        buttonHBox.setAlignment(Pos.BASELINE_CENTER);
-        buttonHBox.setPadding(new Insets(10));
-        buttonHBox.setSpacing(10);
-        buttonHBox.getChildren().add(startAnimation);
-        buttonHBox.getChildren().add(exitAnimation);
-
-
-       //Add the buttons
-        borderPane.setBottom(buttonHBox);
-
-        borderPane.setCenter(animationPane);
-
-
+		animationPane.getChildren().addAll(sun, head, chest, bottom, leftArm, rightArm, heart, leftEye, rightEye, mouth, night, moon);
+		animationPane.setStyle("-fx-background-color: red");
 
 		primaryStage.setTitle("Animate Rectangle");
-
-
-
-    	Scene scene = new Scene(borderPane,SCENE_WIDTH,SCENE_HEIGHT);
+		Scene scene = new Scene(animationPane,SCENE_WIDTH,SCENE_HEIGHT);
 		primaryStage.setScene(scene);
-		primaryStage.setMaxHeight(SCENE_HEIGHT + 50);
-		primaryStage.setMinHeight(SCENE_HEIGHT + 50);
-		primaryStage.setMaxWidth(SCENE_WIDTH);
-		primaryStage.setMinWidth(SCENE_WIDTH);
-    	primaryStage.show();
+		primaryStage.show();
 	}
-	
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		launch(args);
 	}
